cde86899ecd74208dd97610b0e9fb691923553e1
hasecom <hasetennisth@gmail.com>
2019-10-02 23:41:35 +0900

    update

--- Views/Assets/js/index.js
+++ Views/Assets/js/index.js
@@ -1,3 +1,30 @@
-export function sample(){
-    console.log("jhelo")
+
+$(function(){
+    dom();
+});
+
+function dom(){
+    $(".firstView").css({
+        width:$(window).width() + "px",
+        height:$(window).height() + "px",
+    });
 }
+let ajaxParam;
+export function ParamSet(val){
+    ajaxParam = val;
+}
+$(document).on('click','.AjaxSend',function(){
+    $.ajax({
+        url:'./api/first',
+        type:'POST',
+        dataType: 'html',
+        data:{
+            param:ajaxParam
+        }
+    })
+    .done( (data) => {
+        $('body').html(data);
+        dom();
+    })
+})
+
